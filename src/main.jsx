5c009842e22530e5b7ae2a98d9058ee5350cd6f2
Shauna Mack <shaunakatemack@gmail.com>
2024-11-26 10:27:44 +1000

    replace boilerplate code with a basic home and project page

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import HomePage from "./pages/HomePage.jsx";
+import ProjectPage from "./pages/ProjectPage.jsx";
+
+// Create router and tell it what pages to render at what path
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/project",
+    element: <ProjectPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* Wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
