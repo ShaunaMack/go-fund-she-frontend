9c4d8d3192af601a3fd6db4c59a2c06cdc9fc1d5
Shauna Mack <shaunakatemack@gmail.com>
2024-11-26 11:18:06 +1000

    use the fetched projects in the project page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import ProjectCard from "../components/ProjectCard";
-import { allProjects } from "../data";
+import useProjects from "../hooks/use-projects";
 import "./HomePage.css";
 
 function HomePage() {
+  const { projects } = useProjects();
   return (
     <div id="project-list">
-      {allProjects.map((projectData, index) => {
+      {projects.map((projectData, index) => {
         return <ProjectCard key={index} projectData={projectData} />;
       })}
     </div>
