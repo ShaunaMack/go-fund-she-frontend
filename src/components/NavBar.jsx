1f86f0c96117b64427443e53cf72e4e4694d040c
Shauna Mack <shaunakatemack@gmail.com>
2024-11-26 11:30:59 +1000

    remove link to a project page from the nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,6 @@ function NavBar() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/project">Project</Link>
-          </li>
         </ul>
       </nav>
       <Outlet />
