d7b6727e333750eb4a24c3c0d5b9ac16f511f781
Shauna Mack <shaunakatemack@gmail.com>
2024-11-28 17:58:32 +1000

    add the current user to auth

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,5 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import getCurrentUser from "../api/get-current-user";
 
+/* eslint-disable react/prop-types */
 // Here we create the Context
 export const AuthContext = createContext();
 
@@ -9,10 +11,35 @@ export const AuthProvider = (props) => {
   const [auth, setAuth] = useState({
     // Here we initialize the context with the token from local storage, this way if the user refreshes the page we can still have the token in memory.
     token: window.localStorage.getItem("token"),
+    user: null, // Initialize with null to indicate user data is not yet loaded
   });
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Update the user in auth when it's loaded
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      if (!auth.token) {
+        setIsLoading(false);
+        return;
+      }
+
+      try {
+        const userData = await getCurrentUser(auth.token);
+        setAuth((prevAuth) => ({ ...prevAuth, user: userData }));
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserDetails();
+  }, [auth.token]);
+
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, isLoading, error }}>
       {props.children}
     </AuthContext.Provider>
   );
