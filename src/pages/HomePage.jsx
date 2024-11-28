51c9e586c035116fbaa2c05e7a1a0cb203988b6a
Shauna Mack <shaunakatemack@gmail.com>
2024-11-28 16:29:05 +1000

    only show the create project form to users who are logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import ProjectCard from "../components/ProjectCard";
 import ProjectForm from "../components/ProjectForm";
+import { useAuth } from "../hooks/use-auth";
 import useProjects from "../hooks/use-projects";
 import "./HomePage.css";
 
 function HomePage() {
+  const { auth, setAuth } = useAuth();
   const { projects, isLoading, error } = useProjects();
 
   if (isLoading) {
@@ -14,12 +16,18 @@ function HomePage() {
     return <p>{error.message}</p>;
   }
   return (
-    <div id="project-list">
-      {projects.map((projectData, index) => {
-        return <ProjectCard key={index} projectData={projectData} />;
-      })}
-      <ProjectForm />
-    </div>
+    <>
+      <div id="project-list">
+        {projects.map((projectData, index) => {
+          return <ProjectCard key={index} projectData={projectData} />;
+        })}
+      </div>
+      {auth.token ? (
+        <ProjectForm />
+      ) : (
+        <div>Login to create your own project</div>
+      )}
+    </>
   );
 }
 
