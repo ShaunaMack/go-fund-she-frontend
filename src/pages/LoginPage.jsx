cf8fcfa5128c9eca1059f22887df9e4ee3b2529f
Shauna Mack <shaunakatemack@gmail.com>
2024-11-28 16:37:52 +1000

    allow user to toggle between sign up and login forms

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import SignupForm from "../components/SignupForm";
 
 function LoginPage() {
+  // if isLogin show the login form or else show the sign up form
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleForm = () => {
+    setIsLogin((prevIsLogin) => !prevIsLogin);
+  };
+
   return (
-    <>
-      <SignupForm />;
-      <LoginForm />;
-    </>
+    <div>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      {isLogin ? <LoginForm /> : <SignupForm />}
+      <button onClick={toggleForm}>
+        {isLogin
+          ? "Don't have an account? Sign up"
+          : "Already have an account? Log in"}
+      </button>
+    </div>
   );
 }
 
