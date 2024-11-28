13df7bb98470562db623631573b4ba9434e748cd
Shauna Mack <shaunakatemack@gmail.com>
2024-11-28 16:31:58 +1000

    only show the pledge form to users who are logged in

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import useProject from "../hooks/use-project";
 import PledgeForm from "../components/PledgeForm";
+import { useAuth } from "../hooks/use-auth";
 
 function ProjectPage() {
+  const { auth, setAuth } = useAuth();
   // Here we use a hook that comes for free in react router called `useParams` to get the id from the URL so that we can pass it to our useProject hook.
   const { id } = useParams();
   // useProject returns three pieces of info, so we need to grab them all here
@@ -31,7 +33,11 @@ function ProjectPage() {
           );
         })}
       </ul>
-      <PledgeForm projectId={project.id} />
+      {auth.token ? (
+        <PledgeForm projectId={project.id} />
+      ) : (
+        <div>Login to make a pledge to this project</div>
+      )}
     </div>
   );
 }
